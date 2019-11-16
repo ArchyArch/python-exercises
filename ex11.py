2e31f41a5e53a0c1692390618406579e9bc36793
Magdalena Nowowiejska <nowowiejska.magda@gmail.com>
2019-11-16 17:43:44 +0100

    Exercise 11 - input

---
+++ ex11.py
@@ -0,0 +1,15 @@
+print("How old are you?")
+age = input()
+print("How tall are you?")
+height = input()
+print("How much do you weigh?")
+weight = input()
+
+print("So, you're %r old, %r tall and %r heavy." % (age, height, weight))
+
+
+# end = '' w princie kasuje znak nowej linii i kolejny drukuje w tej samej
+
+# print("How tall are you?", end = ' ')
+# height = input()
+# >>> How tall are you? 11
