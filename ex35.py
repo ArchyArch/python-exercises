4ce7249df3330ac9c2cc35e65f9f93df5933f283
Magdalena Nowowiejska <nowowiejska.magda@gmail.com>
2019-12-29 23:27:18 +0100

    Exercise 35 - functions & branches, exit(), dead()

--- ex35.py
+++ ex35.py
@@ -22,3 +22,18 @@ def bear_room():
     print("The fat bear is in front of another door.")
     print("How are you going to move the bear?")
     bear_moved = False
+
+    while True:
+        next = input("> ")
+
+        if next == "take honey":
+            dead("The bear looks at you then slaps your face off.")
+        elif next == "taunt bear" and not bear_moved:
+            print("The bear has moved from the door. You can go through it now.")
+            bear_moved = True
+        elif next == "taunt bear" and bear_moved:
+            dead("The bear gets pissed off and chews your leg off.")
+        elif next == "open door" and bear_moved:
+            gold_room()
+        else:
+            print("I got no idea what that means.")
