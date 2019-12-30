7aaaecf6de41b0429a6066815293ad0f6c2bd0f5
Magdalena Nowowiejska <nowowiejska.magda@gmail.com>
2019-12-31 00:09:20 +0100

    Exercise 35 - functions & branches, exit(), dead()

--- ex35.py
+++ ex35.py
@@ -20,7 +20,7 @@ def bear_room():
     print("There is a bear here.")
     print("The bear has a bunch of honey.")
     print("The fat bear is in front of another door.")
-    print("How are you going to move the bear?")
+    print("How are you going to move the bear: take honey, taunt bear or open door?")
     bear_moved = False
 
     while True:
@@ -38,10 +38,12 @@ def bear_room():
         else:
             print("I got no idea what that means.")
 
+
 def cthulhu_room():
     print("Here you see the great evil Cthulhu.")
     print("He, it, whatever stares at you and you go insane.")
     print("Do you flee for your life or eat your head?")
+    print("'flee' or 'head'?")
 
     next = input("> ")
 
@@ -51,3 +53,25 @@ def cthulhu_room():
         dead("Well that was tasty!")
     else:
         cthulhu_room()
+
+
+def dead(why):
+    print(why, "Good job!")
+    exit(0)
+
+def start():
+    print("You are in a dark room.")
+    print("There is a door to your right and left.")
+    print("Which one do you take?")
+
+    next = input("> ")
+
+    if next == "left":
+        bear_room()
+    elif next == "right":
+        cthulhu_room()
+    else:
+        dead("You stumble around the room until you starve.")
+
+
+start()
